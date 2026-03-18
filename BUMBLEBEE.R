97800eb3ede2aeea13cbf591f6582176c4044c63
z-vickers <zachvickers@uga.edu>
2026-03-18 16:04:43 -0400

    pdf, tuna plot
    change pdf name, plot

--- BUMBLEBEE.R
+++ BUMBLEBEE.R
@@ -1,10 +1,10 @@
 tuna <- read.csv("https://raw.githubusercontent.com/henryjhershey/FORK-ME-CRU/refs/heads/main/TunaData.csv?",header=T)
 
 summary(tuna)
-pdf("YOUR NAME.pdf")
-plot(tuna$catch~tuna$year, type="l",lwd=4 
-     #,color = CHANGE)
-)
+pdf("ZV.pdf")
+plot(tuna$catch~tuna$year, type="l",lwd=6, 
+     color = "green")
+
 lines(tuna$index ~ tuna$year #,lty=CHANGE)
       )
 dev.off()
